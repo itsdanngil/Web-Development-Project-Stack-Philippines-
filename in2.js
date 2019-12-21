5b537deb5e44f18ff9d61bbed9fe2c63739e6c76
Dann Gil Rabaya <55736739+itsdanngil@users.noreply.github.com>
2019-12-21 12:58:24 +0800

    Create in2.js

---
+++ in2.js
@@ -0,0 +1,13 @@
+function openNav() {
+  document.getElementById("sp").style.width = "100px";
+}
+
+function closeNav() {
+  document.getElementById("sp").style.width = "0";
+}
+function openNavq() {
+document.getElementById("txt1").style.display = "block";
+}
+function post10() {
+    document.getElementById("all").innerHTML = post10;
+}
